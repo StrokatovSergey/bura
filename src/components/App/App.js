1319a34bfd3a13df4669ddf0ff28fc146ce2063e
Sergey Strokatov <strokatovsergey@gmail.com>
2019-10-18 16:20:24 +0400

    перераспределил структуру папок

---
+++ src/components/App/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+import AppHeader from '../AppHeader/AppHeader';
+import SearchPanel from '../SearchPanel/SearchPanel';
+import TodoList from '../TodoList/TodoList';
+import ItemStatusFilter from '../ItemStatusFilter/ItemStatusFilter';
+
+import './App.css';
+
+
+const App = () => {
+
+
+    const todoData = [
+        { taskName: 'drink', importantForItem: false, id: 1 },
+        { taskName: 'eat', importantForItem: false, id: 2 },
+        { taskName: 'MAKE APP', importantForItem: true, id: 3 }
+    ];
+
+    return (
+        <div className="todo-app">
+            <AppHeader toDo={1} done={3} />
+            <div className="top-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter />
+            </div>
+
+            <TodoList todoDataFromApp={todoData} />
+        </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/AppHeader';
-import SearchPanel from './components/SearchPanel';
-import TodoList from './components/TodoList';
-import ItemStatusFilter from './components/item-status-filter';
-
-
-
-import './index.css';
-
-
-const App = () => {
-
-
-    const todoData = [
-        { taskName: 'drink', importantForItem: false, id: 1 },
-        { taskName: 'eat', importantForItem: false, id:2 },
-        { taskName: 'MAKE APP', importantForItem: true, id: 3 }
-    ];
-
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
-            </div>
-
-            <TodoList todoDataFromApp={todoData} />
-        </div>
-    )
-}
-
-
+import App from './components/App/App';
 
 
 ReactDOM.render(<App />,
